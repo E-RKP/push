d55f1c74e0ea012a71e6dcdac9a49bb79484267a
E-RKP <edobackup111@gmail.com>
2025-05-21 18:21:25 +0200

    version 4

--- data.js
+++ data.js
@@ -77,3 +77,104 @@ azzerare.onclick = function azzerus (){
   numerino.innerHTML = counterm;
   numerone.innerHTML = counterB;
 }
+
+let dog = {
+name: 'lollipop',
+legs: 4,
+age: [0,1,4,5,7],
+bark: function (){
+  return 'wof wof'
+}
+}
+console.log(dog)
+console.log(dog['age'][2])
+console.log(dog.bark())
+
+dog.breed = 'dogg'
+
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
+
+let npersone = Object.keys(users)
+let max = Number.NEGATIVE_INFINITY
+for (i=0; i<npersone.length;i++){
+
+  let persona = users[npersone[i]]
+  let nskill = persona.skills.length
+  if (nskill > max){
+    max = nskill
+  }
+  
+}
+
+for (i=0; i<npersone.length;i++){
+  if (max === users[npersone[i]].skills.length)
+    console.log(users[npersone[i]])
+}
+
+let cou = 0
+for (i=0; i<npersone.length;i++){
+  if (users[npersone[i]].isLoggedIn === true && users[npersone[i]].points >= 50){
+    cou++;
+  }
+}
+console.log(cou)
+
+let cc = 0
+for (i=0; i<npersone.length;i++){
+  let skiller = users[npersone[i]].skills
+  if (skiller.includes('JavaScript') && skiller.includes('MongoDB') && skiller.includes('Node')){
+    cc++;
+  }
+}
+console.log(cc)
+
